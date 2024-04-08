d510dda2f85cf2de214a9970825044c0d6b5b949
Abhishek Kaushal <abhishekkaushal121@gmail.com>
2024-04-08 12:14:24 +0530

    NavBar initial designing done.

---
+++ components/NavBar.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import Link from "next/link";
+import CodingBracketsLogo from "./svg/CodingBracketsLogo";
+
+const navLinks: { id: number; link: string; href: string }[] = [
+  { id: 1, link: "Home", href: "" },
+  { id: 2, link: "Services", href: "" },
+  { id: 3, link: "Contact us", href: "" },
+  { id: 4, link: "Testimonials", href: "" },
+];
+
+const NavBar = () => {
+  return (
+    <nav className="border-b border-gray-200">
+      <div className="container mx-auto flex w-full justify-between py-1">
+        <div className="flex align-middle space-x-20">
+          <CodingBracketsLogo />
+          <ul className="flex space-x-20 items-center font-semibold text-gray-500 text-xl text-center pt-2">
+            {navLinks.map((navItem) => (
+              <li key={navItem.id} className="hover:text-gray-900">
+                <Link href={navItem.href}>{navItem.link}</Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        <button className="w-fit h-fit my-auto rounded-xl text-white px-8 py-4 bg-cyan-600 font-semibold text-xl hover:bg-white hover:text-cyan-600 hover:ring-1 hover:ring-cyan-600">
+          Free Quote!
+        </button>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
