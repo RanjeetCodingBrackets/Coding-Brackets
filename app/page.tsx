026be6b7944861bec8448989851fc421fb3176e2
Abhishek Kaushal <abhishekkaushal121@gmail.com>
2024-04-10 12:11:15 +0530

    Contact section done.

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import AboutCompanyPage from "@/components/HomePage/AboutCompany";
+import ContactFormSection from "@/components/HomePage/ContactForm";
 import HomePageHeroSection from "@/components/HomePage/HomePageHeroSection";
 import ServiceFeaturesPage from "@/components/HomePage/ServiceFeatures";
 import ServicesPage from "@/components/HomePage/Services";
@@ -13,6 +14,8 @@ export default function Home() {
       <ServicesPage />
 
       <ServiceFeaturesPage />
+
+      <ContactFormSection />
 
       <div className="h-screen"></div>
     </main>
---
+++ components/HomePage/ContactForm.tsx
@@ -0,0 +1,114 @@
+import Link from "next/link";
+import React from "react";
+
+const ContactFormSection = () => {
+  return (
+    <section className="container mx-auto px-16">
+      <div
+        className="bg-top bg-no-repeat bg-cover min-w-full min-h-fit py-10 px-28 rounded-[40px] overflow-hidden"
+        style={{ backgroundImage: "url(/ContactFormBg.png)" }}
+      >
+        <div className="flex flex-col space-y-7 items-center align-middle justify-center">
+          <h5 className="text-white font-extrabold text-2xl">
+            Lets have a project with us!
+          </h5>
+          <h2 className="text-white font-extrabold text-5xl">
+            Let's Discuss Your Project
+          </h2>
+        </div>
+
+        <form action="" className="">
+          <div className="w-full flex pt-10 space-x-20">
+            <div className="flex flex-col w-full space-y-4">
+              <label htmlFor="name" className="text-white font-medium text-xl">
+                Name
+              </label>
+              <input
+                type="text"
+                id="name"
+                placeholder="Enter Your Name"
+                className="px-4 py-5 rounded-lg placeholder:text-[#B3B3B3] placeholder:text-xl placeholder:font-medium outline-none text-gray-700 text-xl font-medium focus:shadow-lg focus:shadow-gray-600 transition-all ease-in-out"
+              />
+            </div>
+
+            <div className="flex flex-col w-full space-y-4">
+              <label htmlFor="email" className="text-white font-medium text-xl">
+                Email
+              </label>
+              <input
+                type="text"
+                id="email"
+                placeholder="Enter Your Email Address"
+                className="px-4 py-5 rounded-lg placeholder:text-[#B3B3B3] placeholder:text-xl placeholder:font-medium outline-none text-gray-700 text-xl font-medium focus:shadow-lg focus:shadow-gray-600 transition-all ease-in-out"
+              />
+            </div>
+          </div>
+
+          <div className="w-full flex pt-10 space-x-20">
+            <div className="flex flex-col w-full space-y-4">
+              <label htmlFor="phone" className="text-white font-medium text-xl">
+                Phone
+              </label>
+              <input
+                type="tel"
+                id="phone"
+                placeholder="Enter Your Phone Number"
+                className="px-4 py-5 rounded-lg placeholder:text-[#B3B3B3] placeholder:text-xl placeholder:font-medium outline-none text-gray-700 text-xl font-medium focus:shadow-lg focus:shadow-gray-600 transition-all ease-in-out"
+              />
+            </div>
+
+            <div className="flex flex-col w-full space-y-4">
+              <label
+                htmlFor="budget"
+                className="text-white font-medium text-xl"
+              >
+                Budget
+              </label>
+              <input
+                type="number"
+                id="budget"
+                placeholder="Enter Your Budget"
+                className="px-4 py-5 rounded-lg placeholder:text-[#B3B3B3] placeholder:text-xl placeholder:font-medium outline-none text-gray-700 text-xl font-medium focus:shadow-lg focus:shadow-gray-600 transition-all ease-in-out"
+              />
+            </div>
+          </div>
+
+          <div className="flex flex-col w-full space-y-4 pt-10">
+            <label
+              htmlFor="services"
+              className="text-white font-medium text-xl"
+            >
+              Services
+            </label>
+            <input
+              type="text"
+              id="services"
+              placeholder="Telecom/ IT Websites"
+              className="px-4 py-5 rounded-lg placeholder:text-[#B3B3B3] placeholder:text-xl placeholder:font-medium outline-none text-gray-700 text-xl font-medium focus:shadow-lg focus:shadow-gray-600 transition-all ease-in-out"
+            />
+          </div>
+
+          <div className="flex flex-col w-full space-y-4 pt-10">
+            <label htmlFor="comment" className="text-white font-medium text-xl">
+              Comment or Message
+            </label>
+            <input
+              type="text"
+              id="comment"
+              placeholder="Write Us a Message"
+              className="px-4 py-5 rounded-lg placeholder:text-[#B3B3B3] placeholder:text-xl placeholder:font-medium outline-none text-gray-700 text-xl font-medium focus:shadow-lg focus:shadow-gray-600 transition-all ease-in-out"
+            />
+          </div>
+
+          <div className="pt-10 w-full flex justify-center align-middle items-center">
+            <button className="text-[#0788A5] font-bold text-xl items-center align-middle px-7 py-4 rounded-xl bg-white hover:bg-[#0788A5] hover:text-white hover:ring-1 hover:ring-white transition-all ease-in-out">
+              <Link href="">Request Proposal</Link>
+            </button>
+          </div>
+        </form>
+      </div>
+    </section>
+  );
+};
+
+export default ContactFormSection;
